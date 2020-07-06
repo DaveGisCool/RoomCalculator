f6ced9868f9ea9530490f75a2d932aedff5b9509
Dave Gedert <DaveGedert@QuickenLoans.com>
2020-07-06 15:38:30 -0400

    This is the first lab work.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace RoomCalculator2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Length: ");
+            string length_entry = Console.ReadLine();
+            double length = double.Parse(length_entry);
+            Console.Write("Enter Width: ");
+            string width_entry = Console.ReadLine();
+            double width = double.Parse(width_entry);
+
+            double roomArea = length * width;
+            Console.WriteLine("Area: " + roomArea);
+            Console.WriteLine("Permiter: " + ((length * 2) + (width * 2)));
+
+            if (roomArea >= 650)
+            {
+                Console.WriteLine("This is a large-sized room.");
+            }
+            else if (roomArea >= 250)
+            {
+                Console.WriteLine("This is a medium-sized room.");
+            }
+            else if (roomArea >= 1)
+            {
+                Console.WriteLine("This is a small-sized room.");
+            }
+
+            Console.WriteLine("Volume: " + (roomArea * 10));
+
+            /*Console.Write("Continue? y/n): ");
+            char userContinue = Console.ReadLine();
+
+            if (userContinue = "n")
+            {
+                Console.WriteLine("Thank you for using the Room Detail Generator!");
+            }
+            else if (userContinue = "y")
+            {
+                Console.WriteLine("Thank you for using the Room Detail Generator!");
+            }
+            */
+        }
+    }
+}
